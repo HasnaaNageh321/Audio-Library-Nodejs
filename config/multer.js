dd32c1f68067064727facddd701359ee878d6409
alaa781 <habibaabobasha@gmail.com>
2025-07-19 01:25:49 +0300

    config

--- config/multer.js
+++ config/multer.js
@@ -20,6 +20,71 @@ const fileFilter = (req, file, cb) => {
   cb(null, isValid);
 };
 
-const upload = multer({ storage, fileFilter });
+const upload = multer({ storage, fileFilter, limits: { fileSize: 2 * 1024 * 1024 } });
 
-module.exports = upload; 
+
+//----------------------------------------------------------------
+//audio configration 
+
+const audioStorage =  multer.diskStorage({
+  destination: (req, file, cb) => {
+    const dir = "uploads/audios";
+    cb(null, dir);
+  },
+  filename: (req, file, cb) => {
+    const ext = path.extname(file.originalname);
+    cb(null, Date.now() + "-audio" + ext);
+  },
+});
+
+
+const audioFilter=(req, file, cb) => {
+  const allowedTypes = [".mp3", ".m4a"];
+  const ext = path.extname(file.originalname).toLowerCase();
+
+  if (allowedTypes.includes(ext)) {
+    cb(null, true);
+  } else {
+    cb(new Error("Only .mp3 and .m4a files are allowed"));
+  }
+};
+
+const uploadedAudio=multer({
+  storage:audioStorage,
+  fileFilter:audioFilter,
+  limits: { fileSize: 50 * 1024 * 1024 } })
+
+//---------------------------------------------------------------------------
+//ccover picture configration
+const coverStorage =  multer.diskStorage({
+  destination: (req, file, cb) => {
+    const dir = "uploads/covers";
+    cb(null, dir);
+  },
+  filename: (req, file, cb) => {
+    const ext = path.extname(file.originalname);
+    cb(null, Date.now() + "-cover" + ext);
+  },
+});
+
+
+const coverFilter=(req, file, cb) => {
+  const allowedTypes = [".jpg", ".png"]
+  const ext = path.extname(file.originalname).toLowerCase();
+
+  if (allowedTypes.includes(ext)) {
+    cb(null, true);
+  } else {
+    cb(new Error("only .jpg and .png files are allowed"));
+  }
+};
+
+const uploadedCover = multer({
+  storage: coverStorage,
+  fileFilter: coverFilter,
+  limits: { fileSize: 5 * 1024 * 1024 }
+});
+
+
+module.exports = {upload,uploadedAudio,uploadedCover} 
+ 
